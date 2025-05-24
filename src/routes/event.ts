73277971062453a327a4c458a36d30d98f389a95
chris-simplified <165951138+chris-simplified@users.noreply.github.com>
2025-05-24 21:09:17 +0100

    update routes

--- src/routes/event.ts
+++ src/routes/event.ts
@@ -7,11 +7,11 @@ const router = Router();
 
 router.post('/',  uploadFields, authMiddleware, EventController.createEvent);
 router.get('/', authMiddleware, EventController.getEvents);
-router.get('/:id', EventController.getEventById);
-router.put('/:id',  uploadFields, authMiddleware, EventController.updateEvent);
-router.delete('/:id', authMiddleware, EventController.deleteEvent);
 router.get('/upcoming', EventController.getUpcomingEvents);
 router.get('/live', authMiddleware, EventController.getLiveEvents);
 router.get('/past', authMiddleware, EventController.getPastEvents);
+router.get('/:id', EventController.getEventById);
+router.put('/:id',  uploadFields, authMiddleware, EventController.updateEvent);
+router.delete('/:id', authMiddleware, EventController.deleteEvent);
 
 export default router;
