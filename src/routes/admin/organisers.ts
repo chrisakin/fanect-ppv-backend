15b2561594c1a652bae0e87da34d7ee64f70f13c
Chris <chrisfemide@gmail.com>
2025-08-02 03:04:38 +0100

    update

--- src/routes/admin/organisers.ts
+++ src/routes/admin/organisers.ts
@@ -4,7 +4,7 @@ import OrganiserController from '../../controllers/admin/organisersController';
 
 const router = Router();
 
-router.get('/all-users', adminAuthMiddleware, OrganiserController.getAllOrganisers);
+router.get('/all-organisers', adminAuthMiddleware, OrganiserController.getAllOrganisers);
 router.get('/single-organiser-events/:id', adminAuthMiddleware, OrganiserController.getEventsCreatedByOrganiser);
 
 export default router;
